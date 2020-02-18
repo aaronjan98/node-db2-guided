4c69e2a8aae59af4a1e63d5f9c86f8c09618e159
aaronjan <aaronjan98@gmail.com>
2020-02-18 11:09:26 -0800

    migrate up and down functions

--- migrations/20200218095517_create_characters_table.js
+++ migrations/20200218095517_create_characters_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('characters', tbl => {
+      tbl.increments();
+      tbl.string('name', 256).notNullable().index();
+      tbl.string('house', 128);
+      tbl.string('origin', 256);
+      tbl.boolean('alive').defaultTo(true);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('characters');
 };
